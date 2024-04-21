48d786d28b7b31f145fad01dbe892be2290dab63
Karma Wangchuk <kwangchuk508@gmail.com>
2024-04-21 18:03:44 +0000

    Revert "Merge branch 'main' of gitlab.com:prj_group1/prj_group_certifi"
    This reverts commit 37552d280b17aabf142ea10032480bc93e72e6f8

--- certifi_client/src/app.jsx
+++ certifi_client/src/app.jsx
@@ -1,95 +1,33 @@
-import React, { useState, useEffect } from "react";
-import Login from "./component/Login"; // Corrected import statement
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
 
-import abi from "./contractJson/Booklist.json";
-import { ethers } from "ethers"; //import ethers library
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./component/Layout";
-import Public from "./component/Public";
-import UserRegister from "./pages/UserRegister";
-import InstitutionRegister from "./pages/InstitutionRegister";
-import ForgotPassword from "./pages/Forgetpassword";
-import UserChoice from "./pages/UserChoice";
-import ChangeP from"./pages/Changep"
-import OtpEnter from "./pages/OtpEnter"
-
-function App() {
-  const [state, setState] = useState({
-    provider: null,
-    signer: null,
-    contract: null,
-  });
-
-  const [account, setAccount] = useState("Not connected");
-
-  useEffect(() => {
-    const template = async () => {
-      //checking if there is metamask installed or not
-      if (!window.ethereum) {
-        alert(
-          "MetaMask is not installed. Please install MetaMask to use this dApp."
-        );
-        return;
-      }
-
-      //1. Fetching the contract details as indicated in step xvi
-      const contractAddress = "0x394c6E6BF7CaE7D4Ace2fB1063B2279857CeFA40";
-      const contractABI = abi.abi;
-
-      //Metamask connection
-      //1. for transaction on sepholia testnets
-      //2. consit of alchemy api which autually help in connecting to the frontend
-      const { ethereum } = window;
-
-      //3.Define provider and signer that will help connect with the blockchain
-      //will be used to read from the blockchain
-      const provider = new ethers.providers.Web3Provider(ethereum);
-
-      //Define signer that will help in transaction to change the blockchain state
-      //wil be used to write to the blockchain
-      const signer = provider.getSigner();
-
-      //4. create the instance of the contract communicate with the smart contract
-      const contract = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      );
-
-      console.log(contract);
-
-      setState({ provider, signer, contract });
-
-      //invoke the metamask wallet
-      const account = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      //reload the window on changing the account
-      window.ethereum.on("Account changed", () => {
-        window.location.reload();
-      });
-      setAccount(account);
-    };
-    template();
-  }, []);
+export function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Public />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/uregister" element={<UserRegister />} />
-        <Route path="/iregister" element={<InstitutionRegister />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} /> 
-        <Route path="/userchoice" element={<UserChoice />} /> 
-        <Route path="/changep" element={<ChangeP />} />
-        <Route path="/otpenter" element={<OtpEnter />} />
-    </Routes>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
     </>
-  );
+  )
 }
-
-export default App;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+require("@nomicfoundation/hardhat-toolbox");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
-  networks: {
-    sepolia: {
-      url: process.env.sepolia_url, // Replace with the actual Sepolia URL
-      accounts: process.env.private_key, // Replace with your private key
-    },
-  }
 };
