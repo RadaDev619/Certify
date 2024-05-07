749a87edbb283426e376bcaad70f356809e12a57
Rada Dorji <12220057.gcit@rub.edu.bt>
2024-05-07 10:51:02 +0600

    changes

--- certifi_client/src/main.jsx
+++ certifi_client/src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from "./App"
-import './../src/css/index.css'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import "./../src/css/index.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-  </React.StrictMode>,
-)
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
