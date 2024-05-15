d0dba480ae98e35d9a0172e25b9ee1e59837f946
KARMA WANGCHUK <kwangchuk508@gmail.com>
2024-05-16 02:56:56 +0600

    Uploading certificate copy file to cloudinary and hash to database

--- certifi_client/src/pages/certificate/certificatevalidation.jsx
+++ certifi_client/src/pages/certificate/certificatevalidation.jsx
@@ -427,14 +427,14 @@ function CertificateValidation() {
             <button
               className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-[45%]"
               onClick={handleAccept}
-              onClick={openVerifyModal}
+              // onClick={openVerifyModal}
             >
               Approve
             </button>
             <button
               className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-[45%]"
               onClick={handleReject}
-              onClick={opennotValidModal}
+              // onClick={opennotValidModal}
             >
               Reject
             </button>
