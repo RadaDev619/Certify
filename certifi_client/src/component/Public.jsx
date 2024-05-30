c6be8b54e50b0129c2d5fd425a3372d856284a63
Rada Dorji <12220057.gcit@rub.edu.bt>
2024-05-30 17:18:37 +0600

    smalChanges

--- certifi_client/src/component/Public.jsx
+++ certifi_client/src/component/Public.jsx
@@ -45,28 +45,29 @@ const Public = () => {
 
     try {
       // // Extract address (42 characters)
-      const address = ID.substring(0, 42);
+      // const address = ID.substring(0, 42);
 
       // Extract identifier (24 characters)
       const identifier = ID.substring(42, 66);
 
       // // Extract hash (remaining characters)
-      const hash = ID.substring(66);
-      setIsLoading(true);
-      const transaction = await contract.getIPFSHash(address, identifier, hash);
+      // const hash = ID.substring(66);
+      // setIsLoading(true);
+      // const transaction = await contract.getIPFSHash(address, identifier, hash);
 
       // console.log("Waiting for transaction...");
       // // alert("reach1");
-      const receipt = await transaction.wait(); // Wait for the transaction to be mined
-      alert("Transaction is Successful!");
-      const event = receipt.events;
-      console.log("Event object:", event);
+      // const receipt = await transaction.wait(); // Wait for the transaction to be mined
+      // alert("Transaction is Successful!");
+      // const event = receipt.events;
+      // console.log("Event object:", event);
       // // Access the concatenatedString from the args array
-      const concatenatedString = event[0].args[0];
-      console.log("Concatenated String:", concatenatedString);
+      // const concatenatedString = event[0].args[0];
+      // console.log("Concatenated String:", concatenatedString);
+      // // alert("Concatenated String: " + concatenatedString);
 
       // // Set the URL state to the concatenated string
-      setSearchResult(concatenatedString);
+      // setSearchResult(concatenatedString);
       const response = await fetch(
         `https://prj-certifi-backend.onrender.com/api/certificate/getCertificatebyId/${identifier}`,
         {
@@ -80,7 +81,7 @@ const Public = () => {
       if (data.status === "success") {
         setFetchedData(data.data); // Set fetched data in state
         Toastify({
-          text: "Transaction successful!",
+          text: "Transaction Successful!",
           duration: 3000,
           close: true,
           gravity: "top",
