53529693e17a8a1aac368bbbab963904811f281e
KARMA WANGCHUK <kwangchuk508@gmail.com>
2024-05-31 05:43:00 +0600

    deployment

--- certifi_client/vite.config.js
+++ certifi_client/vite.config.js
@@ -1,23 +1,22 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import {ethers} from 'ethers';
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [ethers],
       plugins: [
         nodeResolve({
           browser: true,
-          preferBuiltins: true
         }),
         commonjs(),
       ],
     },
   },
   optimizeDeps: {
-    include: [ethers],
+    include: ['ethers'],
   },
 });
